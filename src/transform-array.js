3b642fb9f28aacfbe6832506fc113c59f3083c86
Rz0R <vpa05@mail.ru>
2021-09-28 22:33:07 +1000

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(origArr) {
+
+    if (!Array.isArray(origArr)) {
+        throw Error('\'arr\' parameter must be an instance of the Array!');
+    }
+
+    const arr = origArr.slice();
+    const resArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--double-next' && i + 1 !== arr.length) {
+            resArr.push(arr[i + 1]);
+        } else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined) {
+            resArr.push(arr[i - 1])
+        } else if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined) {
+            resArr.pop();
+        } else if (arr[i] === '--discard-next') {
+            arr[i + 1] = undefined;
+            console.log(arr[i + 1]);
+        } else if (arr[i] !== '--double-prev' && arr[i] !== '--double-next' && arr[i] !== '--discard-prev' && arr[i] !== undefined) {
+            resArr.push(arr[i]);
+        }
+    }
+
+    return resArr;
 }
+
+export default transform;
